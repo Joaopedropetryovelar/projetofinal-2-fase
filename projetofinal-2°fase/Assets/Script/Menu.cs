da11baaa3b7e48667daa32647be78040e306feb2
Joaopedropetryovelar <joaopetryovelar@gmail.com>
2025-11-16 20:48:39 -0300

    Sons

--- projetofinal-2°fase/Assets/Script/Menu.cs
+++ projetofinal-2°fase/Assets/Script/Menu.cs
@@ -7,25 +7,13 @@ public class MenuPrincipalManager : MonoBehaviour
 {
     [SerializeField] private string Jogo;
     [SerializeField] private GameObject painelMenuInicial;
-    [SerializeField] private GameObject painelOpcoes;
+
 
     
 
     public void Jogar()
     {
         SceneManager.LoadScene(Jogo);
-    }
-
-    public void AbrirOpcoes()
-    {
-        painelMenuInicial.SetActive(false);
-        painelOpcoes.SetActive(true);
-    }
-
-    public void FecharOpcoes()
-    {
-        painelOpcoes.SetActive(false);
-        painelMenuInicial.SetActive(true);
     }
 
     public void SairJogo()
